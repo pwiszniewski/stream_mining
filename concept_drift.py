b9893d7b9d37db5dc80bd86ba54e477eb0d26190
pwiszniewski <43343096+pwiszniewski@users.noreply.github.com>
2022-11-30 17:33:05 +0100

    init

--- concept_drift.py
+++ concept_drift.py
@@ -1,68 +1,27 @@
-from river import ensemble
-from river import dummy
-from river import tree
-from river import stream
-from river import metrics
-
-# filePath = './data/weather.arff'
-filePath = './data/cpu.arff'
-
-
-class ModelFactory:
-    def createModel(modelType):
-        #Hoeffdingtrees
-        if modelType=='HT':
-            return tree.HoeffdingTreeClassifier()
-        #Adaptiverandomforest
-        if modelType=='ARF':
-            return ensemble.AdaptiveRandomForestClassifier()
-        #StreamingRandomPatches
-        if modelType=='SRP':
-            return ensemble.SRPClassifier()
-        #NoChangeClassifier
-        if modelType=='NoChange':
-            return dummy.NoChangeClassifier()
-        #MajorityClassclassifier
-        if modelType=='MajorityClass':
-            return dummy.PriorClassifier()
-
-
-modelsToDevelop=['NoChange','MajorityClass', 'HT']
+from river import drift
 import numpy as np
 import matplotlib.pyplot as plt
-plt.figure(figsize=(6,3))
-plt.xlabel('Examples')
-plt.ylabel('Accuracy[%]')
-for modelType in modelsToDevelop:
-    accuracySeries=[]
-    instanceCount=0
-    myStream=stream.iter_arff(filePath,target='class')
-    model=ModelFactory.createModel(modelType)
-    metric=metrics.ClassificationReport()
-    for x,y in myStream:
-        ypred=model.predict_one(x)
-        model.learn_one(x,y)
-        if ypred is not None:
-            metric.update(y,ypred)
-            accuracySeries.append(100*metric._accuracy.get())
-        else:
-            accuracySeries.append(np.nan)
-            instanceCount=instanceCount+1
-    instanceIndexes=np.arange(1,instanceCount+1)
-    # plt.plot(instanceIndexes[::5],accuracySeries[::5],label=modelType)
-    ax1 = plt.subplot(2, 1, 1)
-    ax1.plot(accuracySeries,label=modelType)
 
-    window = 30
-    slidingWindowAccuracy=[]
-    for ind in range(len(accuracySeries)-window+1):
-        slidingWindowAccuracy.append(np.mean(accuracySeries[ind:ind+window ] ) )
-        #initiatefirstwindow-1valueswithnansaswecalculating
-        #movingaverageoutofwindowvaluesyet
-        for ind in range(window-1):
-            slidingWindowAccuracy.insert(0,np.nan)
-
-    ax2 = plt.subplot(2, 1, 2)
-    ax2.plot(slidingWindowAccuracy, label='sliding window')
-plt.legend()
+dataStreamIncremental = np.concatenate([np.full(200,2),np.arange(2,3,1/600),np.full(1200,3)])
+dataStreamSudden = np.concatenate([500*[0], np.full(1800,3)])
+dataStreamReoccurring = np.concatenate([500*[0], np.full(500,3), 1300*[0]])
+dataStreamGradual = np.concatenate([300*[0], np.full(50,3), 300*[0], np.full(150,3), 400*[0], np.full(750,3)])
+# model = drift.ADWIN()
+# model = drift.KSWIN()
+model = drift.HDDM_A()
+# datastream = dataStreamIncremental
+# datastream = dataStreamSudden
+# datastream = dataStreamReoccurring
+datastream = dataStreamGradual
+plt.figure(figsize=(12,3))
+x = list(range(len(datastream)))[::20]
+y = datastream[::20],
+plt.scatter(x, y, s=5);
+plt.xlabel('Time')
+#adaptedfromriverexample
+for i,val in enumerate(datastream):
+    _ = model.update(val)
+    if model.drift_detected:
+        print(f'Change detected at example{i},input value at which change was detected:{val}')
+        plt.axvline(x=i,color='r')
 plt.show()
--- drift_detection.py
+++ drift_detection.py
@@ -1,12 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from river import drift
+from river import metrics
 from river import ensemble
 from river import dummy
 from river import tree
 from river import stream
-from river import metrics
 
-# filePath = './data/weather.arff'
 filePath = './data/cpu.arff'
-
 
 class ModelFactory:
     def createModel(modelType):
@@ -26,44 +27,61 @@ class ModelFactory:
         if modelType=='MajorityClass':
             return dummy.PriorClassifier()
 
-
-modelsToDevelop=['NoChange','MajorityClass', 'HT']
-import numpy as np
-import matplotlib.pyplot as plt
+modelsToDevelop=['HT']
+showInlinePlots=True
+window=50
+adwin=drift.ADWIN()
+kswin=drift.KSWIN()
+HDDMA=drift.HDDM_A()
+HDDMW=drift.HDDM_W()
 plt.figure(figsize=(6,3))
 plt.xlabel('Examples')
 plt.ylabel('Accuracy[%]')
+
+plotName=''
 for modelType in modelsToDevelop:
     accuracySeries=[]
+    plotName=modelType+''+plotName
     instanceCount=0
     myStream=stream.iter_arff(filePath,target='class')
+
     model=ModelFactory.createModel(modelType)
     metric=metrics.ClassificationReport()
     for x,y in myStream:
         ypred=model.predict_one(x)
+        wrongPrediction = 0 if ypred==y else 1
+
+        adwin.update(wrongPrediction)
+        kswin.update(wrongPrediction)
+        HDDMA.update(wrongPrediction)
+        HDDMW.update(wrongPrediction)
+
+        if adwin.drift_detected:
+            plt.axvline(x=instanceCount,color='r')
+        if kswin.drift_detected:
+            plt.axvline(x=instanceCount,color='b')
+        if HDDMA.drift_detected:
+            plt.axvline(x=instanceCount,color='k')
+        if HDDMW.drift_detected:
+            plt.axvline(x=instanceCount,color='g')
+
         model.learn_one(x,y)
         if ypred is not None:
             metric.update(y,ypred)
-            accuracySeries.append(100*metric._accuracy.get())
+            accuracySeries.append(100*(1-wrongPrediction))
         else:
             accuracySeries.append(np.nan)
-            instanceCount=instanceCount+1
-    instanceIndexes=np.arange(1,instanceCount+1)
-    # plt.plot(instanceIndexes[::5],accuracySeries[::5],label=modelType)
-    ax1 = plt.subplot(2, 1, 1)
-    ax1.plot(accuracySeries,label=modelType)
 
-    window = 10
+        instanceCount=instanceCount+1
+    instanceIndexes=np.arange(1,instanceCount+1)
+
     slidingWindowAccuracy=[]
     for ind in range(len(accuracySeries)-window+1):
-        # print(ind)
-        slidingWindowAccuracy.append(np.mean(accuracySeries[ind:ind+window ] ) )
-        #initiatefirstwindow-1valueswithnansaswecalculating
-        #movingaverageoutofwindowvaluesyet
+        slidingWindowAccuracy.append(np.mean(accuracySeries[ind:ind+window]))
     for ind in range(window-1):
         slidingWindowAccuracy.insert(0,np.nan)
-    print(len(slidingWindowAccuracy))
-    ax2 = plt.subplot(2, 1, 2)
-    ax2.plot(slidingWindowAccuracy, label='sliding window')
+    # plt.plot(instanceIndexes[::5],slidingWindowAccuracy[::5],label=modelType)
+    plt.plot(slidingWindowAccuracy,label=modelType)
 plt.legend()
 plt.show()
+    
